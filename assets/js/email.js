7098758523551f962fed393cbc29e2a06aed96b2
Samantha Lastig <slastig13@gmail.com>
2019-12-26 16:17:37 -0500

    photos in place

--- assets/js/email.js
+++ assets/js/email.js
@@ -3,14 +3,14 @@ const button = document.getElementById('sendMessageButton')
 button.addEventListener('click', evt => {
   evt.preventDefault()
 
-  let email = document.getElementById('email').value
-  let sender = document.getElementById('name').value
+  let user_email = document.getElementById('email').value
+  let user_name = document.getElementById('name').value
   let content = document.getElementById('message').value
 
-  if (email && sender && content) {
+  if (user_email && user_name && content) {
     let templateParams = {
-      email,
-      sender,
+      user_email,
+      user_name,
       content
     }
 
@@ -29,17 +29,21 @@ button.addEventListener('click', evt => {
     document.getElementById('name').value = ''
     document.getElementById('message').value = ''
 
-    location.hash = 'thanks'
-    setTimeout(function() {
-      location.hash = ''
-    }, 5000)
     if (document.getElementById('addedNode')) {
       document.getElementById('addedNode').remove()
     }
+    let successNode = document.createElement('P')
+    successNode.innerText =
+      "Thank's for reaching out! An email has been sent to me and I'll be in touch as soon as possible. \n All the best, \n Sam"
+    successNode.id = 'successNode'
+    document.getElementById('contact-form').appendChild(successNode)
   } else if (!document.getElementById('addedNode')) {
-    let node = document.createElement('LI')
+    if (document.getElementById('successNode')) {
+      document.getElementById('successNode').remove()
+    }
+    let node = document.createElement('P')
     node.innerText = 'Please fill out all fields before submitting!'
     node.id = 'addedNode'
-    document.getElementById('contactActions').appendChild(node)
+    document.getElementById('contact-form').appendChild(node)
   }
 })
